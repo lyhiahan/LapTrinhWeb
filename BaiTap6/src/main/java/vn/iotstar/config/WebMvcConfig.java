2d96be5b6be2c0f0b06d989614e65d2b344a2d64
Ly Gia Han <lygiahan.786@gmail.com>
2026-09-14 11:15:50 +0700

    BaiTap6

--- BaiTap6/src/main/java/vn/iotstar/config/WebMvcConfig.java
+++ BaiTap6/src/main/java/vn/iotstar/config/WebMvcConfig.java
@@ -19,18 +19,31 @@ public class WebMvcConfig implements WebMvcConfigurer {
             uploadBasePath += "/";
         }
 
+        File baiTap4Upload = new File("../BaiTap4/upload").getAbsoluteFile();
+        String baiTap4Path = baiTap4Upload.getAbsolutePath().replace("\\", "/");
+        if (!baiTap4Path.endsWith("/")) {
+            baiTap4Path += "/";
+        }
+
         registry.addResourceHandler("/upload/**")
                 .addResourceLocations(
                         "file:" + uploadBasePath,
                         "file:" + uploadBasePath + "category/",
                         "file:" + uploadBasePath + "product/",
+                        "file:" + uploadBasePath + "avatar/",
+                        "file:" + baiTap4Path,
+                        "file:" + baiTap4Path + "avatar/",
+                        "file:" + baiTap4Path + "product/",
+                        "file:d:/LAPTRINH/LAPTRINHWEB/BAITAP/BT/LapTrinhWeb/BaiTap4/upload/",
+                        "file:d:/LAPTRINH/LAPTRINHWEB/BAITAP/BT/LapTrinhWeb/BaiTap4/upload/avatar/",
                         "file:upload/",
                         "file:upload/category/",
                         "file:upload/product/",
+                        "file:upload/avatar/",
                         "classpath:/static/upload/",
                         "classpath:/static/upload/category/",
-                        "classpath:/static/upload/product/"
-                );
+                        "classpath:/static/upload/product/",
+                        "classpath:/static/upload/avatar/");
 
         registry.addResourceHandler("/static/**")
                 .addResourceLocations("classpath:/static/");
